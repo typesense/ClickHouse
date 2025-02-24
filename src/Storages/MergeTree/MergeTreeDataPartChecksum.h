f3865313e53646da3daad69d356800b7e38a7e7c
Sema Checherinda <checherinda@gmail.com>
2025-02-24 15:01:30 +0100

    remove records about dropped projection from checksums.txt

--- src/Storages/MergeTree/MergeTreeDataPartChecksum.h
+++ src/Storages/MergeTree/MergeTreeDataPartChecksum.h
@@ -89,7 +89,7 @@ struct MergeTreeDataPartChecksums
     UInt64 getTotalSizeOnDisk() const;
     UInt64 getTotalSizeUncompressedOnDisk() const;
 
-    String getDebugString() const;
+    Strings getFileNames() const;
 };
 
 /// A kind of MergeTreeDataPartChecksums intended to be stored in ZooKeeper (to save its RAM)
